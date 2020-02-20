3447a1f84eeb1a2ddb959a77342b289fe9e6107d
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-02-20 00:42:56 +0000

    CSP voilations

--- src/components/card/style.js
+++ src/components/card/style.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Title = styled.h2`
   font-size: 1.5em;
+  font-weight: 700;
   text-align: center;
-  color: palevioletred;
 `;
 
 export const CardImg = styled.div`
