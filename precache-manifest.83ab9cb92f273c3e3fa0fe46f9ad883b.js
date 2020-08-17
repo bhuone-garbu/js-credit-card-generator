cbcb17f54b56c54fe3c12ab85e796c504c72761f
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-08-17 23:34:41 +0100

    Updates

---
+++ precache-manifest.83ab9cb92f273c3e3fa0fe46f9ad883b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73635e7b24273c4e73b59c0db1efc325",
+    "url": "/js-credit-card-generator/index.html"
+  },
+  {
+    "revision": "560cc82fdbd099d32929",
+    "url": "/js-credit-card-generator/static/css/main.a1cec0e0.chunk.css"
+  },
+  {
+    "revision": "0f16f837f12364458d12",
+    "url": "/js-credit-card-generator/static/js/2.40df6f69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-credit-card-generator/static/js/2.40df6f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560cc82fdbd099d32929",
+    "url": "/js-credit-card-generator/static/js/main.cefe987c.chunk.js"
+  },
+  {
+    "revision": "d76578eb862b1d793f9d",
+    "url": "/js-credit-card-generator/static/js/runtime-main.7597bcfe.js"
+  },
+  {
+    "revision": "3a070c4727cdd6fc182ded0463c14748",
+    "url": "/js-credit-card-generator/static/media/amex.3a070c47.svg"
+  },
+  {
+    "revision": "67a87acb7b14cd644eb26091a56c34b1",
+    "url": "/js-credit-card-generator/static/media/discover.67a87acb.svg"
+  },
+  {
+    "revision": "5966f60b11e12eca6c0919de95891436",
+    "url": "/js-credit-card-generator/static/media/mastercard.5966f60b.svg"
+  },
+  {
+    "revision": "67f273f02358390c0349adaa8dcfec7c",
+    "url": "/js-credit-card-generator/static/media/visa.67f273f0.svg"
+  }
+]);
